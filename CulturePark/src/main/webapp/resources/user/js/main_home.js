7ff05b20fb99bd95e8c0e3126368835bf22d551d
Jinheeya0913 <taran0913@naver,com>
2022-05-11 18:19:57 +0900

    created eventsection slide

--- CulturePark/src/main/webapp/resources/user/js/main_home.js
+++ CulturePark/src/main/webapp/resources/user/js/main_home.js
@@ -1,5 +1,24 @@
 $(document).ready(function () {
 
+
+    // 섹션 이미지
+
+    $('.img_list img').css('width', $('.slide_section').width());
+    var img_height = $('.img_list img ').height();
+    $('.slide_section').css('min-height', img_height);
+
+    $(window).resize(function () {
+
+        // console.log('이미지 높이 : '+$('.img_list img').height());
+        // console.log('섹션 높이 : '+$('.slide_section').height());
+
+        $('.slide_section').css('min-height', $('.img_list img').height());
+        $('.img_list img').css('width', $('.slide_section').width());
+
+    });
+
+    //버튼 슬라이드 동작
+
     var imgs;
     var img_count;
     var img_position = 1;
@@ -20,59 +39,26 @@ $(document).ready(function () {
 
         var imgs_width = $('.img_list').width();
 
-        console.log('---------------')
-        console.log('시작 ')
-        console.log('ul의 너비는 ' + $('.img_box').width());
-        console.log('이미지의 크기는 '+imgs_width);
-        console.log('이미지의 count는 '+img_count);
-        console.log('이미지의 위치는' + img_position);
         if (1 < img_position) {
             imgs.animate({
                 left: '+='+imgs_width+'px'
             });
             img_position--;
+            $('#evt_chk_0' + img_position).prop("checked", true);
         }
-
-        console.log('---------------')
-
-        console.log('이미지의 크기는 '+imgs_width);
-        console.log('이미지의 count는 '+img_count);
-        console.log('이미지의 위치는' + img_position);
-
     }
 
     function next() {
 
         var imgs_width = $('.img_list').width();
 
-        console.log('---------------')
-        console.log('시작 ')
-        console.log('이미지의 크기는 '+imgs_width);
-        console.log('이미지의 count는 '+img_count);
-        console.log('이미지의 위치는' + img_position);
-
         if (img_count > img_position) {
-            console.log('실행1');
             imgs.animate({
                 left:'-='+imgs_width+'px'
             });
-
             img_position++;
-
+            $('#evt_chk_0' + img_position).prop("checked", true);
 
         }
-
-        console.log('---------------')
-
-        console.log('이미지의 크기는 '+imgs_width);
-        console.log('이미지의 count는 '+img_count);
-        console.log('이미지의 위치는' + img_position);
-
     }
-
-
-
-
-
-
 });
