ecbc4cfe7b859d667cc4f4a2df44147d70385ec1
Jinheeya0913 <taran0913@naver,com>
2022-05-12 16:48:09 +0900

    0512 main

--- CulturePark/src/main/webapp/resources/user/js/main_home.js
+++ CulturePark/src/main/webapp/resources/user/js/main_home.js
@@ -6,6 +6,11 @@ $(document).ready(function () {
     var img_height = $('.img_list img ').height();
     $('#slide_event').css('min-height', img_height);
 
+
+    $('.prom_list_m img').css('width', $('.slide_section_m').width());
+    var m_img_height = $('.prom_list_m img').height();
+    $('.slide_section_m').css('min-height', m_img_height);
+
     // $('.prom_list img').css('width', $('#slide_ex').width());
     // var ex_height = $('.prom_list img').height();
     // $('#slide_ex').css('min-height', ex_height);
@@ -16,6 +21,9 @@ $(document).ready(function () {
 
         $('#slide_event').css('min-height', $('.img_list img').height());
         $('.img_list img').css('width', $('#slide_event').width());
+
+        $('.slide_section_m').css('min-height', $('.prom_list_m img').height());
+        $('.prom_list_m img').css('width', $('.slide_section_m').width());
 
     });
 
@@ -40,7 +48,6 @@ $(document).ready(function () {
     function prev() {
 
         var imgs_width = $('.img_list').width();
-
         if (1 < img_position) {
             imgs.animate({
                 left: '+='+imgs_width+'px'
@@ -68,7 +75,8 @@ $(document).ready(function () {
 
     var ex_prom_number=1;
     var mu_prom_number=1;
-
+    var prom_img_position_ex = 1;
+    var prom_img_position_mu = 1;
 
     $('.prom_prev').click(function (){
         slidePrev();
@@ -82,22 +90,34 @@ $(document).ready(function () {
 
         var window_width = $(window).width()    /* 윈도우크기*/
         var node = $('#' + nodename);           /* ul 변수*/
+        console.log(node);
 
         var prom_img_count = node.children().length;
-        var prom_img_position = 1;
         var prom_list_width =  $('#'+nodename + ' > li' ).width();
 
-        if (window_width < 500) {
-            var move_width = window_width;
-
-        } else if (window_width<1200){
-            var move_width = window_width;
-        }
-
         if(window_width<1200){
+            console.log("현재 위치 : " + prom_img_position_ex);
+            console.log("요소 갯수 : " + prom_img_count);
+            console.log("리스트 길이 : " + prom_list_width);
+            console.log('요소 이름 : ' + nodename);
+
+            if (prom_img_count > prom_img_position_ex && nodename==='ex_prom_m') {
+                node.animate({
+                    left:'-='+prom_list_width+'px'
+                })
+                prom_img_position_ex++;
+                $('#ex_chk_0' + prom_img_position_ex).prop("checked", true);
+            } else if (prom_img_count > prom_img_position_mu && nodename==='mu_prom_m'){
+                node.animate({
+                    left:'-='+prom_list_width+'px'
+                })
+                prom_img_position_mu++;
+                $('#mu_chk_0' + prom_img_position_mu).prop("checked", true);
+            }
 
 
-        }else{
+        } else{
+
             var move_width = 1200;
 
             if(ex_prom_number===1&&nodename==='ex_prom') {
@@ -121,34 +141,53 @@ $(document).ready(function () {
     }
 
     function slidePrev() {
-        var window_width = $(window).width()
-        var node = $('#' + nodename);
 
-        if (window_width < 500) {
-            var move_width = window_width;
 
-        } else if (window_width<1200){
-            var move_width = window_width;
+        var window_width = $(window).width()    /* 윈도우크기*/
+        var node = $('#' + nodename);           /* ul 변수*/
 
-        } else if (1200<=window_width) {
+
+        var prom_img_count = node.children().length;
+        var prom_list_width =  $('#'+nodename + ' > li' ).width();
+
+        if(window_width<1200){
+            console.log("현재 위치 : " + prom_img_position_ex);
+            console.log("요소 갯수 : " + prom_img_count);
+            console.log("리스트 길이 : " + prom_list_width);
+            console.log('요소 이름 : ' + nodename);
+
+            if (1 < prom_img_position_ex && nodename==='ex_prom_m') {
+                node.animate({
+                    left:'+='+prom_list_width+'px'
+                })
+                prom_img_position_ex--;
+                $('#ex_chk_0' + prom_img_position_ex).prop("checked", true);
+            } else if (1 < prom_img_position_mu && nodename==='mu_prom_m'){
+                node.animate({
+                    left:'+='+prom_list_width+'px'
+                })
+                prom_img_position_mu--;
+                $('#mu_chk_0' + prom_img_position_mu).prop("checked", true);
+            }
+
+
+        } else{
+
             var move_width = 1200;
-        }
 
+            if (ex_prom_number === 2 && nodename==='ex_prom') {
+                node.animate({
+                    left:'+='+move_width+'px'
+                })
+                ex_prom_number=1;
+            }
 
-
-
-        if (ex_prom_number === 2 && nodename==='ex_prom') {
-            node.animate({
-                left:'+='+move_width+'px'
-            })
-            ex_prom_number=1;
-        }
-
-        if (mu_prom_number === 2 && nodename === 'mu_prom') {
-            node.animate({
-                left:'+='+move_width+'px'
-            })
-            mu_prom_number=1;
+            if (mu_prom_number === 2 && nodename === 'mu_prom') {
+                node.animate({
+                    left:'+='+move_width+'px'
+                })
+                mu_prom_number=1;
+            }
         }
 
 
