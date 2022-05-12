682504138a7d365790982c8f57617363900e6261
blisssing <blisssing@naver.com>
2022-05-13 02:08:24 +0900

    0513 / 0208 / myTicket

--- CulturePark/src/main/java/kg/twojin/culturePark/user/controller/MyPageController.java
+++ CulturePark/src/main/java/kg/twojin/culturePark/user/controller/MyPageController.java
@@ -1,16 +1,25 @@
 package kg.twojin.culturePark.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MyPageController {
 
-@RequestMapping(value="/myPage.do")
-    public ModelAndView goToMypage(){
+    @RequestMapping(value = "/myPage.do")
+    public ModelAndView goToMypage() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("myPage");
         return mv;
     }
+
+    @RequestMapping(value = "/myTicket.do")
+    public ModelAndView goToMyTicket() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("myTicket");
+        return mv;
+    }
+
 }
