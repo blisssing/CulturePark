0b8841a74cf36bba3632c3de3701edde37f3cf33
Jinheeya0913 <taran0913@naver,com>
2022-06-27 14:11:27 +0900

    0627 맵핑 경로 옮김

--- CulturePark/src/main/java/kg/twojin/culturePark/common/dao/impl/ProdoctDAOImpl.java
+++
@@ -1,9 +0,0 @@
-package kg.twojin.culturePark.common.dao.impl;
-
-import kg.twojin.culturePark.common.dao.ProdoctDAO;
-import org.springframework.stereotype.Repository;
-
-@Repository("prodocutDAO")
-public class ProdoctDAOImpl implements ProdoctDAO {
-
-}
